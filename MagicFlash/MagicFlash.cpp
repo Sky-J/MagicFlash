96c1b06b1ffaeb56bc28ad3b01f0b74c99da60b7
Sky-J <460545197@qq.com>
2013-08-30 14:28:41 +0800

    let's make it be able to control the swf

---
+++ MagicFlash/MagicFlash.cpp
@@ -0,0 +1,255 @@
+// MagicFlash.cpp : 定义应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "MagicFlash.h"
+#include "FlashWnd.h"
+
+#define MAX_LOADSTRING 100
+
+// 全局变量:
+HINSTANCE hInst;								// 当前实例
+TCHAR szTitle[MAX_LOADSTRING];					// 标题栏文本
+TCHAR szWindowClass[MAX_LOADSTRING];			// 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+HWND g_hWnd = NULL;
+HINSTANCE g_hInst = NULL;
+CFlashWnd *g_flashWnd = NULL;
+HICON g_hIcon = NULL;
+NOTIFYICONDATA nid;
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: 在此放置代码。
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// 初始化全局字符串
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_MAGICFLASH, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// 执行应用程序初始化:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MAGICFLASH));
+
+	OleInitialize(NULL);	
+	g_flashWnd = new CFlashWnd;
+
+	g_flashWnd->Create(ShockwaveFlashObjects::CLSID_ShockwaveFlash,
+		WS_EX_LAYERED, WS_POPUP | WS_VISIBLE | WS_CLIPSIBLINGS,
+		g_hWnd, g_hInst);
+
+	
+
+	// 主消息循环:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	delete g_flashWnd;
+
+	OleUninitialize();
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目的: 注册窗口类。
+//
+//  注释:
+//
+//    仅当希望
+//    此代码与添加到 Windows 95 中的“RegisterClassEx”
+//    函数之前的 Win32 系统兼容时，才需要此函数及其用法。调用此函数十分重要，
+//    这样应用程序就可以获得关联的
+//    “格式正确的”小图标。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MAGICFLASH));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_MAGICFLASH);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	g_hIcon = wcex.hIcon;
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目的: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_EX_TOOLWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   //ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   g_hWnd = hWnd;
+
+   return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的: 处理主窗口的消息。
+//
+//  WM_COMMAND	- 处理应用程序菜单
+//  WM_PAINT	- 绘制主窗口
+//  WM_DESTROY	- 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+	
+	UINT WM_TASKBARCREATED;
+	// 不要修改TaskbarCreated，这是系统任务栏自定义的消息
+	WM_TASKBARCREATED = RegisterWindowMessage(TEXT("TaskbarCreated"));
+
+	switch (message)
+	{
+
+	case WM_CREATE:
+		{
+			//添加相应的系统托盘
+			nid.cbSize = sizeof(nid);
+			nid.hWnd = hWnd;
+			nid.uID = 0;
+			nid.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP | NIF_INFO;
+			nid.uCallbackMessage = WM_USER;
+			nid.hIcon = g_hIcon;
+			nid.dwState = NIS_SHAREDICON;
+			nid.uTimeout = 5000;
+			nid.dwInfoFlags = NIIF_INFO;   
+			lstrcpy(nid.szTip, L"MagicFlash");
+			lstrcpy(nid.szInfoTitle, L"MagicFlash");
+			lstrcpy(nid.szInfo, L"未加载Flash");
+			Shell_NotifyIcon(NIM_ADD, &nid);
+		}
+		break;
+	case WM_USER:
+		{
+			if (lParam == WM_LBUTTONDBLCLK)
+			{
+				SetForegroundWindow(g_flashWnd->m_hWnd);
+			}
+			if (lParam == WM_RBUTTONDOWN)
+			{
+				SetForegroundWindow(hWnd);
+				SendMessage(g_flashWnd->m_hWnd, WM_RBUTTONDOWN, wParam, lParam);
+			}
+		}
+		break;
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 分析菜单选择:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), g_flashWnd->m_hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: 在此添加任意绘图代码...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		Shell_NotifyIcon(NIM_DELETE, &nid);
+		PostQuitMessage(0);
+		break;
+	default:
+		if(message == WM_TASKBARCREATED)
+		{
+			SendMessage(hWnd, WM_CREATE, wParam, lParam);
+			break;
+		}
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
---
+++ MagicFlash/Resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MagicFlash.rc
+//
+#define IDC_MYICON                      2
+#define IDD_MAGICFLASH_DIALOG           102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MAGICFLASH                  107
+#define IDI_SMALL                       108
+#define IDC_MAGICFLASH                  109
+#define IDR_MAINFRAME                   128
+#define IDR_MENU_RIGHT_POP              129
+#define ID_RIGHTMENU_LOAD               32775
+#define ID_RIGHTMENU_TOP                32776
+#define ID_RIGHTMENU_ABOUT              32777
+#define ID_RIGHTMENU_EXIT               32778
+#define ID_RIGHTMENU_SIZE               32780
+#define ID_RIGHTMENU_QUARTER            32786
+#define ID_RIGHTMENU_HALF               32787
+#define ID_RIGHTMENU_                   32788
+#define ID_RIGHTMENU_THREE_QUARTER      32789
+#define ID_RIGHTMENU_NORMAL             32790
+#define ID_RIGHTMENU_DOUBLE             32791
+#define ID_RIGHTMENU_LOW                32796
+#define ID_RIGHTMENU_MID                32797
+#define ID_RIGHTMENU_HIGH               32798
+#define ID_RIGHTMENU_32799              32799
+#define ID_RIGHTMENU_REPLAY             32800
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32801
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
